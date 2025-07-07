d3147b3bb372bee0209d0b36e64de472532243f5
RedstoneFX <zatmonhp@yandex.ru>
2025-07-07 14:59:02 +0300

    Осмысленное приветствие

--- handlers/onStartCommand.py
+++ handlers/onStartCommand.py
@@ -9,11 +9,12 @@ class onStartCommandHandler(CommandHandler):
     @staticmethod
     async def onStart(update: Update, context: ContextTypes.DEFAULT_TYPE):
         keyboard = [
-            [
-                InlineKeyboardButton("Option 1", callback_data='1'),
-                InlineKeyboardButton("Option 2", callback_data='2'),
-            ],
-            [InlineKeyboardButton("Option 3", callback_data='3')],
+            [InlineKeyboardButton("Создать единоразовое уведомление", callback_data='make_single')],
+            [InlineKeyboardButton("Создать интервальное уведомление", callback_data='make_interval')],
+            [InlineKeyboardButton("Просмотреть подключенные уведомления", callback_data='list')],
         ]
         reply_markup = InlineKeyboardMarkup(keyboard)
-        await update.message.reply_text('Please choose:', reply_markup=reply_markup)
+        await update.message.reply_text('Здравствуй, пользователь! Я - бот, управляющий доставкой уведомлений о '
+                                        'приеме лекарств и посещений врача. Вы можете установить напоминания '
+                                        'самостоятельно, либо при помощи специалиста, который определит все параметры '
+                                        'приема за вас. Буду рад помочь сохранить ваше здоровье!', reply_markup=reply_markup)
