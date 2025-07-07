8009b0f8de6fced59cfaef5a75ec936d5df3e200
RedstoneFX <zatmonhp@yandex.ru>
2025-07-07 12:49:53 +0300

    Простейший планировщик

---
+++ scheduler.py
@@ -0,0 +1,17 @@
+from queue import PriorityQueue
+import time
+from typing import Callable
+
+
+class Scheduler:
+    def __init__(self):
+        self.__queue = PriorityQueue()
+
+    def addTask(self, runTime: int, handler: Callable, args: tuple):
+        self.__queue.put((runTime, handler, args))
+
+    def runExpired(self):
+        now = round(time.time())
+        while not self.__queue.empty() and self.__queue.queue[0][0] <= now:
+            item = self.__queue.get()
+            item[1](*item[2], expireTime=item[0])
