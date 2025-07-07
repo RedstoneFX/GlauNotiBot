72d74c4336f1fa7977ddc85ed1965de7f4fc05fb
RedstoneFX <zatmonhp@yandex.ru>
2025-07-07 12:16:47 +0300

    Бот-повторюшка

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import logging
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
 
 with open("token.txt", mode="r", encoding="utf-8") as f:
     TOKEN = f.read()
@@ -8,11 +8,17 @@ with open("token.txt", mode="r", encoding="utf-8") as f:
 logging.basicConfig(format='[%(asctime)s] [%(levelname)s] [%(name)s] %(message)s', level=logging.INFO)
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def onStartCommand(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
 
+async def onMessageReceived(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+
 application = ApplicationBuilder().token(TOKEN).build()
-start_handler = CommandHandler('start', start)
+start_handler = CommandHandler('start', onStartCommand)
+message_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), onMessageReceived)
 application.add_handler(start_handler)
+application.add_handler(message_handler)
 application.run_polling()
