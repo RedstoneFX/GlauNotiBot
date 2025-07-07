eff1537368fd8dabd5138e78c00e4e92c6aa7992
RedstoneFX <zatmonhp@yandex.ru>
2025-07-07 13:43:50 +0300

    Фикс ошибки при совпадении времени нескольких задач

--- scheduler.py
+++ scheduler.py
@@ -7,17 +7,19 @@ from CycledThread import CycledThread
 
 class Scheduler:
     def __init__(self, autorunInterval=5):
+        self.__queueInserted = 0
         self.__autorunThread = CycledThread(autorunInterval, Scheduler.runExpired, (self,))
         self.__queue = PriorityQueue()
 
     def addTask(self, runTime: int, handler: Callable, args=tuple()):
-        self.__queue.put((runTime, handler, args))
+        self.__queue.put((runTime, self.__queueInserted, handler, args))
+        self.__queueInserted += 1
 
     def runExpired(self):
         now = round(time())
         while not self.__queue.empty() and self.__queue.queue[0][0] <= now:
             item = self.__queue.get()
-            item[1](*item[2], expireTime=item[0])
+            item[2](*item[3], expireTime=item[0])
 
     def setAutorunEnabled(self, isEnabled):
         if self.__autorunThread.is_alive() and not isEnabled:
@@ -28,6 +30,8 @@ class Scheduler:
 
 sched = Scheduler(1)
 
+sched.addTask(round(time()) + 5, lambda expireTime: print(expireTime))
+sched.addTask(round(time()) + 5, lambda expireTime: print(expireTime))
 sched.addTask(round(time()) + 5, lambda expireTime: print(expireTime))
 
 sched.setAutorunEnabled(True)
