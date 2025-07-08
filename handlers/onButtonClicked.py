e7c729ad4da6ebd7f8ed1873956ebf271395f067
RedstoneFX <zatmonhp@yandex.ru>
2025-07-08 14:46:21 +0300

    Фикс: await без async в методах onButtonClicked

--- handlers/onButtonClicked.py
+++ handlers/onButtonClicked.py
@@ -14,16 +14,16 @@ class onButtonClickedHandler(CallbackQueryHandler):
         await query.answer()
         if UserManager.getUser(update.effective_chat).isAdmin:
             if query.data == "get_users":
-                onButtonClickedHandler.sendUserList(update.effective_chat)
+                await onButtonClickedHandler.sendUserList(update.effective_chat)
             elif query.data == "add_notification":
-                onButtonClickedHandler.sendAddNotifMenu(update.effective_chat)
+                await onButtonClickedHandler.sendAddNotifMenu(update.effective_chat)
 
     @staticmethod
-    def sendAddNotifMenu(effective_chat):
-        pass
+    async def sendAddNotifMenu(chat):
+        await chat.send_message("")
 
     @staticmethod
-    def sendUserList(chat):
+    async def sendUserList(chat):
         msg = "Пользователи:"
         for user in UserManager.users.values():
             msg += f"\n@{user.name} [{user.chatID}]" + (" (admin)" if user.isAdmin else "")
