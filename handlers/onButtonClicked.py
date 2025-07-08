4b609fa67ea2ff09a202c3de7c1f93a9ce9806c4
RedstoneFX <zatmonhp@yandex.ru>
2025-07-08 14:45:10 +0300

    Вынес отправку списка пользователей в метод

--- handlers/onButtonClicked.py
+++ handlers/onButtonClicked.py
@@ -14,14 +14,18 @@ class onButtonClickedHandler(CallbackQueryHandler):
         await query.answer()
         if UserManager.getUser(update.effective_chat).isAdmin:
             if query.data == "get_users":
-                msg = "Пользователи:"
-                for user in UserManager.users.values():
-                    msg += f"\n@{user.name} [{user.chatID}]" + (" (admin)" if user.isAdmin else "")
-                await update.effective_chat.send_message(msg)
+                onButtonClickedHandler.sendUserList(update.effective_chat)
             elif query.data == "add_notification":
                 onButtonClickedHandler.sendAddNotifMenu(update.effective_chat)
 
     @staticmethod
     def sendAddNotifMenu(effective_chat):
         pass
+
+    @staticmethod
+    def sendUserList(chat):
+        msg = "Пользователи:"
+        for user in UserManager.users.values():
+            msg += f"\n@{user.name} [{user.chatID}]" + (" (admin)" if user.isAdmin else "")
+        await chat.send_message(msg)
     
