77ae75743e7566057ef0ba65ea883336c3d30129
RedstoneFX <zatmonhp@yandex.ru>
2025-07-10 20:58:59 +0300

    Рабочие группы вопросов

--- misc/generate_question_buttons.py
+++ misc/generate_question_buttons.py
@@ -9,7 +9,10 @@ def generate_groups_buttons_markup(lang: str):
     for group in groups:
         replica = lang.get(group)
         if replica:
-            buttons.append([InlineKeyboardButton(replica.body, callback_data="ask_group.by_name" + group)])
+            name = replica.body
+        else:
+            name = group
+        buttons.append([InlineKeyboardButton(name, callback_data="ask_group.by_name." + group)])
     buttons.append([InlineKeyboardButton("Покажи все вопросы", callback_data="ask_group.list_all")])
     return InlineKeyboardMarkup(buttons)
 
@@ -20,4 +23,5 @@ def generate_group_buttons_markup(group_name: str, lang: str, debug_all: bool=Fa
         if "group" in replica.extra and "title" in replica.extra:  # TODO: надо бы переименовать group в ask_group, чтобы избежать путаницы - этот тег только у вопросов должен быть
             if replica.extra["group"] == group_name or debug_all:
                 buttons.append([InlineKeyboardButton(replica.extra["title"], callback_data="ask." + replica.name)])
+    buttons.append([InlineKeyboardButton("<- Назад", callback_data="ask_buttons")])
     return InlineKeyboardMarkup(buttons)
